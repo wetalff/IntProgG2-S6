94defca7e3f2452c544ef1f703aaf8a847130fe2
wetalff <alfredito.osejo@gmail.com>
2025-04-29 19:40:53 -0600

    Ejercicio semana 6 seison 2

---
+++ Sesion 2/ejercicio1.py
@@ -0,0 +1,14 @@
+#tabla de multiplicar del 1 al 10
+def TablaMult(numero):
+    print(f"Tabla de multiplicar del {numero}: ")
+    for i in  range(1,11):
+        respuesta = numero * i
+        print(f"{numero} * {i} = {respuesta}")
+   
+   
+def main():        
+    num = int(input("Ingrese el numero para ver su tabla de multiplicar: "))
+    TablaMult(num)
+
+
+main()
---
+++ Sesion 2/ejercicio2.py
@@ -0,0 +1,16 @@
+#numeros pares
+def NumPares(limite):
+    print(f"Numeros pares hasta {limite}: ")
+    for contador in range(0, limite +1):
+        if contador % 2 == 0:
+            print(contador)
+def main():
+    limite = int(input("Ingrese el numero limite para contar los pares antes de el: "))
+    NumPares(limite)
+    
+main()
+
+       
+        
+     
+   
